a1714ffc9c6effc9d0aa616ed45e6ce6b436b316
Ongezwa122 <Ngotanao@gmail.com>
2025-05-14 11:47:31 +0200

    submitting Assignment week5

--- A5_javas.js
+++ A5_javas.js
@@ -1,1 +1,9 @@
-
+// Basic JS using inline HTML references
+function changeText() {
+    titleText.innerHTML = "🌿 Empowering Rural Communities";
+    titleText.style.color = "darkgreen";
+    message.innerHTML = "Content updated successfully!";
+    message.style.backgroundColor = "#e0f2f1";
+    message.style.padding = "10px";
+    message.style.marginTop = "10px";
+  }
